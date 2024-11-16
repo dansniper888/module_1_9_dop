3a0666c4aa89272c85ae55f37a70f88ae5ff0939
dansniper888 <dansniper888@gmail.com>
2024-11-16 13:30:06 +0700

    Add files via upload

---
+++ module_1_9 dop.py
@@ -0,0 +1,7 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+my_dic = {}
+names = list(students)
+names.sort()
+my_dic.update({names[0]:sum(grades[0])/len(grades[0]), names[1]:sum(grades[1])/len(grades[1]), names[2]:sum(grades[2])/len(grades[2]), names[3]:sum(grades[3])/len(grades[3]), names[4]:sum(grades[4])/len(grades[4])})
+print('Средние баллы учеников -', my_dic)
